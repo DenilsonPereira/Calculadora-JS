c1d125d5f1bb43342ff761c79e01b7714833bc48
DenilsonPereira <denilsongunner@gmail.com>
2022-08-01 10:03:29 -0300

    create calculadora v.1.0
    Criação da calculadora versão 1.0

---
+++ script.js
@@ -0,0 +1,28 @@
+const resultado = document.querySelector('.res');
+const confirmar = document.querySelector('.igual');
+const btn = document.getElementById('mode')
+
+btn.addEventListener("change", (e) => {
+    document.body.classList.toggle("dark", e.target.checked);
+})
+
+function insert(valor){
+    resultado.innerHTML+= valor;
+}
+
+function clean(){
+    resultado.innerHTML = '';
+}
+
+function backspace(){
+    if(resultado.textContent){
+        let result = document.getElementById('resultado').innerHTML
+        resultado.innerHTML = result.substring(0, result.length - 1);
+    }
+}
+function confirma(){
+    if(resultado.textContent != 'Erro'){
+        document.getElementById('resultado').innerHTML = eval(resultado.innerHTML);
+
+    }
+}
